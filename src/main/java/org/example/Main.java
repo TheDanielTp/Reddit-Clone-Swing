611d1e627ce8a53c9ff3cfb378da10dc2749a36c
TheDanielTp <prof.danial4@gmail.com>
2024-03-24 14:03:20 +0330

    Created Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.example.Menu.FrontPageGuestMenu;
+
+import java.util.Scanner;
+
 public class Main
 {
+    public static Scanner scanner = new Scanner (System.in);
+
     public static void main (String[] args)
     {
-
+        new FrontPageGuestMenu ();
+        scanner.nextLine ();
+        Post.displayAllPosts ();
     }
 }
