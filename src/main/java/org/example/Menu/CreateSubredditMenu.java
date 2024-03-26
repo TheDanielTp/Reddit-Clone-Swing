b9159784c82c94468c9c275023acd622c2e7c6c6
TheDanielTp <prof.danial4@gmail.com>
2024-03-26 16:33:54 +0330

    Updated Create Subreddit Menu

--- src/main/java/org/example/Menu/CreateSubredditMenu.java
+++ src/main/java/org/example/Menu/CreateSubredditMenu.java
@@ -4,14 +4,19 @@ import org.example.Subreddit;
 import org.example.User;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class CreateSubredditMenu extends JFrame
 {
     protected JTextField titleField;
-    protected JTextArea descriptionArea;
-    protected JCheckBox over18Checkbox;
+    protected JTextArea  descriptionArea;
+    protected JCheckBox  over18Checkbox;
+    protected JLabel     characterCountLabelTitle;
 
     public CreateSubredditMenu ()
     {
@@ -29,17 +34,46 @@ public class CreateSubredditMenu extends JFrame
         gridBagConstraints.anchor = GridBagConstraints.CENTER; //center the grid bag on window
         gridBagConstraints.insets = new Insets (5, 5, 5, 5); //set grid bag size
 
-        gridBagConstraints.gridx = 0; //sets the horizontal grid position for the component being added to the container
-        gridBagConstraints.gridy = 0; //sets the vertical grid position for the component being added to the container
-
         JPanel textFieldsPanel = new JPanel (); //create text fields panel
         textFieldsPanel.setLayout (new BoxLayout (textFieldsPanel, BoxLayout.Y_AXIS)); //set the layout to box layout
 
         JLabel titleLabel = new JLabel ("Title:");
         titleLabel.setAlignmentX (Component.CENTER_ALIGNMENT); //center the title label
 
-        titleField = new JTextField (10); //set the number of columns to 10
-        titleField.setMaximumSize (new Dimension (400, 25)); //set the maximum size to limit the width and height
+        JPanel titlePanel = new JPanel (); //create a panel for title and current length
+        titlePanel.setLayout (new BoxLayout (titlePanel, BoxLayout.X_AXIS)); //set the layout to box layout
+
+        titleField = new JTextField (10);
+        titleField.setMaximumSize (new Dimension (400, 25));
+        setMaxLength (titleField); // Limit title field to 32 characters
+
+        characterCountLabelTitle = new JLabel (" 0/32"); //add a label to show input limit
+        characterCountLabelTitle.setAlignmentX (Component.CENTER_ALIGNMENT); //center the label
+
+        titleField.getDocument ().addDocumentListener (new DocumentListener ()
+        {
+            @Override
+            public void insertUpdate (DocumentEvent e)
+            {
+                updateCharacterCount (); //update characters count when a character is entered
+            }
+
+            @Override
+            public void removeUpdate (DocumentEvent e)
+            {
+                updateCharacterCount (); //update characters count when a character is removed
+            }
+
+            @Override
+            public void changedUpdate (DocumentEvent e)
+            {
+                updateCharacterCount (); //update characters count when a character is changed
+            }
+        });
+
+        //add the components to title panel
+        titlePanel.add (titleField);
+        titlePanel.add (characterCountLabelTitle);
 
         JLabel descriptionLabel = new JLabel ("Description:");
         descriptionLabel.setAlignmentX (Component.CENTER_ALIGNMENT); //center the description label
@@ -49,17 +83,17 @@ public class CreateSubredditMenu extends JFrame
 
         JScrollPane scrollPane = new JScrollPane (descriptionArea); //create a scroll pane for description text area
 
-        over18Checkbox = new JCheckBox("18+ year old community");
-        over18Checkbox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        over18Checkbox = new JCheckBox ("18+ year old community"); //create checkbox for nsfw community
+        over18Checkbox.setAlignmentX (Component.CENTER_ALIGNMENT); //center the checkbox
 
-        //add the components to the panel
+        //add the components to text fields panel
         textFieldsPanel.add (titleLabel);
-        textFieldsPanel.add (titleField);
+        textFieldsPanel.add (titlePanel);
         textFieldsPanel.add (descriptionLabel);
         textFieldsPanel.add (scrollPane);
         textFieldsPanel.add (over18Checkbox);
 
-        JPanel buttonsPanel = getjPanel (); //create buttons panel
+        JPanel buttonsPanel = getButtonsPanel (); //create buttons panel
 
         //add panels to the main panel
         mainPanel.add (optionsPanel, BorderLayout.NORTH);
@@ -70,14 +104,35 @@ public class CreateSubredditMenu extends JFrame
         setVisible (true); //make the frame visible
     }
 
-    private JPanel getjPanel ()
+    private void setMaxLength (JTextField textField)
+    {
+        textField.setDocument (new PlainDocument ()
+        {
+            @Override //controls how text is inserted
+            public void insertString (int offset, String string, AttributeSet attributeSet)
+                    throws BadLocationException //throws exception if offset is invalid
+            {
+                if (string == null)
+                {
+                    return; //return if title is currently empty
+                }
+
+                if ((getLength () + string.length ()) <= 32) //checks if title and input will have a total of less than 32 characters
+                {
+                    super.insertString (offset, string, attributeSet); //allows the string to be inserted
+                }
+            }
+        });
+    }
+
+    private JPanel getButtonsPanel ()
     {
         JPanel buttonsPanel = new JPanel (new FlowLayout (FlowLayout.CENTER)); //create buttons panel
 
         JButton returnButton = new JButton ("Return to Front Page");
         returnButton.setBackground (new Color (0x0079d3)); //set button color to blue
         returnButton.setForeground (new Color (0xffffff)); //set text color to white
-        returnButton.addActionListener (e ->
+        returnButton.addActionListener (e -> //add action to the button
         {
             dispose (); //close the current frame
             new FrontPageMenu (); //open front page menu
@@ -86,15 +141,19 @@ public class CreateSubredditMenu extends JFrame
         JButton subredditButton = new JButton ("Create Subreddit");
         subredditButton.setBackground (new Color (0xff4500)); //set button color to orange
         subredditButton.setForeground (new Color (0xffffff)); //set text color to white
-        subredditButton.addActionListener (e ->
+        subredditButton.addActionListener (e -> //add action to the button
         {
-            String title = titleField.getText (); //get title from title field
-            String description = descriptionArea.getText (); //get description from description area
-            boolean nsfw = over18Checkbox.isSelected ();
+            String title = titleField.getText (); //gets subreddit's title from title field
+            String description = descriptionArea.getText (); //gets subreddit's description from description field
+            boolean nsfw = over18Checkbox.isSelected (); //gets nsfw boolean from the checkbox
 
             if (title.isEmpty ()) //if no title is entered
             {
                 JOptionPane.showMessageDialog (null, "Please enter a Title.");
+            }
+            else if (! validateTitle (title))
+            {
+                JOptionPane.showMessageDialog (null, "This Title is already taken. Please enter a new Title.");
             }
             else if (description.isEmpty ()) //if no description is entered
             {
@@ -107,14 +166,13 @@ public class CreateSubredditMenu extends JFrame
                 {
                     User user = User.getCurrentUser (); //get the current user
 
-                    Subreddit subreddit = new Subreddit (title, description, user, nsfw);
-                    Subreddit.addSubreddit (subreddit);
+                    Subreddit subreddit = new Subreddit (title, description, user, nsfw); //create new subreddit
+                    Subreddit.addSubreddit (subreddit); //add subreddit to all subreddits list
 
                     dispose (); //close the current frame
                     JOptionPane.showMessageDialog (null, "Subreddit created successfully!");
                     new FrontPageMenu (); //open front page menu
                 }
-
             }
         });
 
@@ -123,6 +181,31 @@ public class CreateSubredditMenu extends JFrame
         buttonsPanel.add (subredditButton);
 
         return buttonsPanel;
+    }
+
+    private boolean validateTitle (String title)
+    {
+        for (Subreddit subreddit : Subreddit.getAllSubreddits ())
+        {
+            if (subreddit.getTitle ().equalsIgnoreCase (title))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void updateCharacterCount ()
+    {
+        int currentLength = titleField.getText ().length ();
+        if (currentLength < 10)
+        {
+            characterCountLabelTitle.setText ("  " + currentLength + "/32"); //print two spaces if length is less than 10
+        }
+        else
+        {
+            characterCountLabelTitle.setText (" " + currentLength + "/32"); //print one space if length is more than 10
+        }
     }
 
     //main function for testing
